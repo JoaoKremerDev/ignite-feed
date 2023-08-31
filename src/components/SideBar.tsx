22cbfa16b826b847124f7f5f540fed2c28619757
JoaoKremerDev <joaokremer.dev@gmail.com>
2023-08-30 21:23:35 -0300

    feat: sidebar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './SideBar.module.css'
 import { PencilLine } from '@phosphor-icons/react';
 
@@ -8,7 +9,7 @@ function SideBar() {
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt=""
             />
             <div className={styles.profile}>
-                <img src="https://github.com/JoaoKremerDev.png" alt="" />
+                <Avatar src="https://github.com/JoaoKremerDev.png" />
                 <strong>João Vitor Kremer</strong>
                 <span>Web Developer</span>
 
@@ -18,8 +19,6 @@ function SideBar() {
                         Editar seu perfil</a>
                 </footer>
             </div>
-
-
         </aside>
     )
 }
